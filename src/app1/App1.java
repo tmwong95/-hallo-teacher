13c6a7bfbc1af80a3facdf29cb377baa2c17395d
Student <Student@PC81>
2016-12-07 12:36:47 +0800

    Hallo

---
+++ src/app1/App1.java
@@ -0,0 +1,12 @@
+package app1;
+
+public class App1 {
+      public static void main (String[] args){
+            int a=3, b=2, c=1;
+            if (a>c){
+                a = b = c = 1;
+
+                System.out.println(b);
+                          }
+}
+}
